77997ea2f08b95f9b860d8bc38f5060401656c21
Борис Московкин <74050819+MoskovkinBoris@users.noreply.github.com>
2021-10-13 22:14:33 +0300

    Add files via upload

---
+++ lab_17/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace lab17 {
+				partial class Form1 {
+								/// <summary>
+								/// Обязательная переменная конструктора.
+								/// </summary>
+								private System.ComponentModel.IContainer components = null;
+
+								/// <summary>
+								/// Освободить все используемые ресурсы.
+								/// </summary>
+								/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+								protected override void Dispose(bool disposing) {
+												if(disposing && (components != null)) {
+																components.Dispose();
+												}
+												base.Dispose(disposing);
+								}
+
+								#region Код, автоматически созданный конструктором форм Windows
+
+								/// <summary>
+								/// Требуемый метод для поддержки конструктора — не изменяйте 
+								/// содержимое этого метода с помощью редактора кода.
+								/// </summary>
+								private void InitializeComponent() {
+												this.components = new System.ComponentModel.Container();
+												this.toggleAnimationButton = new System.Windows.Forms.Button();
+												this.colorDialog = new System.Windows.Forms.ColorDialog();
+												this.chooseCOlorBtn = new System.Windows.Forms.Button();
+												this.mainTImer = new System.Windows.Forms.Timer(this.components);
+												this.SuspendLayout();
+												// 
+												// toggleAnimationButton
+												// 
+												this.toggleAnimationButton.Location = new System.Drawing.Point(618, 25);
+												this.toggleAnimationButton.Name = "toggleAnimationButton";
+												this.toggleAnimationButton.Size = new System.Drawing.Size(115, 23);
+												this.toggleAnimationButton.TabIndex = 0;
+												this.toggleAnimationButton.Text = "Toggle animation";
+												this.toggleAnimationButton.UseVisualStyleBackColor = true;
+												// 
+												// chooseCOlorBtn
+												// 
+												this.chooseCOlorBtn.Location = new System.Drawing.Point(618, 84);
+												this.chooseCOlorBtn.Name = "chooseCOlorBtn";
+												this.chooseCOlorBtn.Size = new System.Drawing.Size(115, 23);
+												this.chooseCOlorBtn.TabIndex = 1;
+												this.chooseCOlorBtn.Text = "Choose color";
+												this.chooseCOlorBtn.UseVisualStyleBackColor = true;
+												// 
+												// mainTImer
+												// 
+												this.mainTImer.Interval = 500;
+												// 
+												// Form1
+												// 
+												this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+												this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+												this.ClientSize = new System.Drawing.Size(800, 450);
+												this.Controls.Add(this.chooseCOlorBtn);
+												this.Controls.Add(this.toggleAnimationButton);
+												this.Name = "Form1";
+												this.Text = "Form1";
+												this.ResumeLayout(false);
+
+								}
+
+								#endregion
+
+								private System.Windows.Forms.Button toggleAnimationButton;
+								private System.Windows.Forms.ColorDialog colorDialog;
+								private System.Windows.Forms.Button chooseCOlorBtn;
+								private System.Windows.Forms.Timer mainTImer;
+				}
+}
+
---
+++ lab_17/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab17 {
+				public partial class Form1 : Form {
+								private Point location = new Point(50, 50);
+								private int Radius = 100;
+								private static int center = 100;
+								private static int step = 75;
+								Boolean isDropStarted = false;
+
+								private Figure xAxis;
+								private Figure yAxis;
+
+								private Figure ellipse;
+								private Figure lineDiagonal;
+								private Figure lineSide;
+								private Figure lineUpper;
+
+								Rectangle boundings = new Rectangle(new Point(center, center), new Size(step * 2, step * 2));
+								Figure pie = new Figure();
+
+
+								public Form1() {
+												InitializeComponent();
+												Paint += Form1_Paint;
+												MouseDown += Form1_MouseDown;
+												MouseUp += RedrawElips;
+												MouseMove += RedrawElips;
+												MouseUp += Form1_MouseUp;
+								}
+
+								private void Form1_MouseUp(object sender, MouseEventArgs e) {
+												isDropStarted = false;
+								}
+
+								private void RedrawElips(object sender, MouseEventArgs e) {
+												if (isDropStarted) {
+																location.X = e.X + Radius / 2;
+																location.Y = e.Y + Radius / 2;
+																Invalidate();
+												}
+												Refresh();
+								}
+
+								private void Form1_MouseDown(object sender, MouseEventArgs e) {
+												int currentX = e.X;
+												int currentY = e.Y;
+												Boolean isInX = (currentX >= location.X - Radius && currentX <= location.X + Radius);
+												Boolean isInY = (currentY >= location.Y - Radius && currentY <= location.Y + Radius);
+
+												if (isInX && isInY) {
+																isDropStarted = true;
+												}
+								}
+
+								private void Form1_Paint(object sender, PaintEventArgs eventArgs) {
+												Graphics graphics = eventArgs.Graphics;
+												Pen filling = new Pen(Color.Black, 2);
+												Pen axisFilling = new Pen(Color.Gray, 2);
+												
+												ellipse = new Figure();
+												ellipse.Path.AddEllipse(new Rectangle(center, center, step * 2, step * 2));
+												lineDiagonal = new Figure();
+												RectangleF ellipseBoundings = ellipse.Path.GetBounds();
+												lineDiagonal.Path.AddLine(center + step * 2, ellipseBoundings.Location.Y, ellipseBoundings.Location.X, center + step * 2);
+												lineUpper = new Figure();
+												lineUpper.Path.AddLine(center, ellipseBoundings.Top, center + step * 2 + 20, ellipseBoundings.Left);
+												lineSide = new Figure();
+												lineSide.Path.AddLine(ellipseBoundings.Right, ellipseBoundings.Top - 20, ellipseBoundings.Right, ellipseBoundings.Bottom);
+
+												int start = 20;
+												float length = (float) (step * 4.5);
+												int centerPoint = center + step;
+												graphics.DrawLine(axisFilling, start, centerPoint, length, centerPoint);
+												graphics.DrawLine(axisFilling, centerPoint, start, centerPoint, length);
+												graphics.DrawEllipse(filling, center, center, step * 2, step * 2);
+												graphics.DrawLine(filling, center + step * 2, ellipseBoundings.Location.Y, ellipseBoundings.Location.X, center + step * 2);
+												graphics.DrawLine(filling, center, ellipseBoundings.Top, center + step * 2 + 20, ellipseBoundings.Left);
+												graphics.DrawLine(filling, ellipseBoundings.Right, ellipseBoundings.Top - 20, ellipseBoundings.Right, ellipseBoundings.Bottom);
+
+												
+												pie.Path.AddPie(boundings, 90, 45);
+												Point pos = PointToClient(Cursor.Position);	
+												bool isMouseIn = pie.Path.IsVisible(pos);
+
+
+												graphics.FillPie(new SolidBrush(isMouseIn ? Color.Green : Color.Blue), boundings, 90, 45);
+								}
+
+								class Figure {
+												public GraphicsPath Path = new GraphicsPath();
+
+												public PointF Center {
+																get {
+																				var rect = Path.GetBounds();
+																				return new PointF(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+																}
+												}
+
+												public Region Region {
+																get { return new Region(Path); }
+												}
+								}
+				}
+}
